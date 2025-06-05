aff157f693b93e304c1da15e43a09f4837c74bdc
Jiří Strnadel <Juzba88@gmail.com>
2025-06-05 16:48:50 +0200

    Přidejte soubory projektu.

---
+++ FutureValueMVC/Models/FutureValueModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FutureValueMVC.Models
+{
+    public class FutureValueModel
+    {
+        public decimal Investment { get; set; }
+
+        public decimal YearlyRate { get; set; }
+
+        public int YearsNumber { get; set; }
+
+        public decimal FutureValue { get; set; }
+    }
+}
